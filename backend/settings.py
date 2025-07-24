25f1699fd0abb34aa7234c91b0c448e7e5a277e0
Md Hasanuzzaman <dr.has82@gmail.com>
2025-07-24 18:29:31 +0600

    Image deleted

--- backend/settings.py
+++ backend/settings.py
@@ -136,6 +136,13 @@ STATICFILES_DIRS = [BASE_DIR / "backend" / "static"]
 # STATIC_ROOT = BASE_DIR / "staticfiles"  # For production use, uncomment this line to collect static files
 
 
+# Media files (User-uploaded content)
+# https://docs.djangoproject.com/en/5.0/topics/files/   
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
